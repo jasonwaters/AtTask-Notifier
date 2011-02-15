c4ad0d3703e04d1879f3a8c8648d0c0f6135a41c
Jason Waters <jason@myheck.net>
2011-02-15 10:29:12 -0700

    very basic firefox extension started.  status bar icon visible.

---
+++ Firefox/chrome/content/overlay.js
@@ -0,0 +1,21 @@
+var attasknotify = {
+  onLoad: function() {
+    // initialization code
+    this.initialized = true;
+    this.strings = document.getElementById("attasknotify-strings");
+  },
+
+  onMenuItemCommand: function(e) {
+    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+                                  .getService(Components.interfaces.nsIPromptService);
+    promptService.alert(window, this.strings.getString("helloMessageTitle"),
+                                this.strings.getString("helloMessage"));
+  },
+
+  onToolbarButtonCommand: function(e) {
+    // just reuse the function above.  you can change this, obviously!
+    attasknotify.onMenuItemCommand(e);
+  }
+};
+
+window.addEventListener("load", function () { attasknotify.onLoad(); }, false);
---
+++ Firefox/defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.attasknotify.boolpref", false);
+pref("extensions.attasknotify.intpref", 0);
+pref("extensions.attasknotify.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.waters@attask.com.description", "chrome://attasknotify/locale/overlay.properties");
