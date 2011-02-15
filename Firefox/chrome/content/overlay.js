3d6d2cacbf735cad07847c77e8fb2639d701662c
Jason Waters <jason@myheck.net>
2011-02-15 11:44:07 -0700

    login window

--- Firefox/chrome/content/overlay.js
+++ Firefox/chrome/content/overlay.js
@@ -1,21 +1,62 @@
+// var AtTaskNotifier = new Class({
+// 	Implements: [Options, Events],
+// 	options: {
+// 	},
+// 		
+// 	initialize: function(options) {
+// 		this.setOptions(options);
+// 	},
+// 	
+// 	onLoad: function() {
+// 		// initialization code
+// 		this.initialized = true;
+// 		this.strings = document.getElementById("attasknotify-strings");
+// 		// document.getElementById("at-notifier-statusbar").setAttribute('label', 'hi bobbo');
+// 	},
+// 	
+// 	login: function() {
+// 		alert('i like mootools');
+// 	}
+// 	
+// });
+
+// var attasknotify = new AttaskNotifier();
+
 var attasknotify = {
-  onLoad: function() {
-    // initialization code
-    this.initialized = true;
-    this.strings = document.getElementById("attasknotify-strings");
-  },
+    onLoad: function() {
+        // initialization code
+        this.initialized = true;
+        this.strings = document.getElementById("attasknotify-strings");
+        document.getElementById("at-notifier-statusbar").setAttribute('label', 'hi bobbo');
+    },
 
-  onMenuItemCommand: function(e) {
-    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                                  .getService(Components.interfaces.nsIPromptService);
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                this.strings.getString("helloMessage"));
-  },
+    onMenuItemCommand: function(e) {
+        var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+        .getService(Components.interfaces.nsIPromptService);
+        promptService.alert(window, this.strings.getString("helloMessageTitle"),
+        this.strings.getString("helloMessage"));
+    },
 
-  onToolbarButtonCommand: function(e) {
-    // just reuse the function above.  you can change this, obviously!
-    attasknotify.onMenuItemCommand(e);
-  }
+    onToolbarButtonCommand: function(e) {
+        // just reuse the function above.  you can change this, obviously!
+        attasknotify.onMenuItemCommand(e);
+    },
+
+    login: function(event) {
+		if(event && event.button == 2) {
+			return;
+		}
+		
+		this.openLoginWindow();
+	},
+	
+	openLoginWindow: function() {
+		this.login_window = window.openDialog("chrome://attasknotify/content/at-login.xul", "_blank", "chrome,resizable=yes,dependent=yes");
+	}
 };
 
-window.addEventListener("load", function () { attasknotify.onLoad(); }, false);
+window.addEventListener("load",
+function() {
+    attasknotify.onLoad();
+},
+false);
